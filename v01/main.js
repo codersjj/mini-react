4859c2b0787973f3428dacc46b2575db5defb682
codersjj <shanewestlife@outlook.com>
2024-01-13 19:11:16 +0800

    refactor(01-实现最简 mini-react): 在页面中呈现 app v0.4 使用 VDOM，VDOM 动态生成，DOM 动态渲染 抽取 App

--- v01/main.js
+++ v01/main.js
@@ -1,7 +1,7 @@
 // 目标：在页面中呈现 app
 
-import React from './core/React.js'
 import ReactDOM from "./core/ReactDOM.js"
+import App from './App.js'
 
 // v0.4 使用 VDOM，VDOM 动态生成，DOM 动态渲染
 // react -> VDOM -> JavaScript 对象
@@ -9,7 +9,7 @@ import ReactDOM from "./core/ReactDOM.js"
 //#region -------------------- 动态生成 VDOM --------------------
 
 // const tNode = createTextNode('app')
-const App = React.createElement('div', { id: 'app' }, 'app')
+// const App = React.createElement('div', { id: 'app' }, 'app')
 // const App = createElement('div', { id: 'app' }, 'hi-', 'mini-react')
 // console.log(App)
 //#endregion -------------------- 动态生成 VDOM --------------------
