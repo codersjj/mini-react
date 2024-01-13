57d04c9a1a6dbc3a835c6effd180c9bb215ad938
codersjj <shanewestlife@outlook.com>
2024-01-13 16:29:23 +0800

    feat(01-实现最简 mini-react): 在页面中呈现 app v0.2 使用 VDOM，VDOM 写死，DOM 渲染写死

--- v01/main.js
+++ v01/main.js
@@ -1,11 +1,44 @@
 // 目标：在页面中呈现 app
 
-// v0.1 不用 VDOM，DOM 渲染写死
-const divEl = document.createElement('div')
+// v0.2 使用 VDOM，VDOM 写死，DOM 渲染写死
+// react -> VDOM -> JavaScript 对象
+// type、props、children
+// const app = {
+//   type: 'div',
+//   props: {
+//     id: 'app',
+//     children: [
+//       {
+//         type: 'TEXT_ELEMENT',
+//         props: {
+//           nodeValue: 'app',
+//           children: []
+//         }
+//       }
+//     ]
+//   }
+// }
+const tNode = {
+  type: 'TEXT_ELEMENT',
+  props: {
+    nodeValue: 'app',
+    children: []
+  }
+}
+const app = {
+  type: 'div',
+  props: {
+    id: 'app',
+    children: [tNode]
+  }
+}
+
+const divEl = document.createElement(app.type)
+divEl.id = app.props.id
 const rootDiv = document.querySelector('#root')
 rootDiv.append(divEl)
 
 const textNode = document.createTextNode('')
-textNode.nodeValue = 'app'
-// const textNode = document.createTextNode('app')
+textNode.nodeValue = tNode.props.nodeValue
+// const textNode = document.createTextNode(tNode.props.nodeValue)
 divEl.append(textNode)
