956205490f4353fe5298459ab8e0d3cf5e538fcf
codersjj <shanewestlife@outlook.com>
2024-01-13 19:02:48 +0800

    refactor(01-实现最简 mini-react): 在页面中呈现 app v0.4 使用 VDOM，VDOM 动态生成，DOM 动态渲染 分离框架代码和业务代码

--- v01/main.js
+++ v01/main.js
@@ -1,33 +1,15 @@
 // 目标：在页面中呈现 app
 
+import React from './core/React.js'
+import ReactDOM from "./core/ReactDOM.js"
+
 // v0.4 使用 VDOM，VDOM 动态生成，DOM 动态渲染
 // react -> VDOM -> JavaScript 对象
 // type、props、children
 //#region -------------------- 动态生成 VDOM --------------------
-const createTextNode = text => {
-  return {
-    type: 'TEXT_ELEMENT',
-    props: {
-      nodeValue: text,
-      children: []
-    }
-  }
-}
-const createElement = (type, props, ...children) => {
-  return {
-    type,
-    props: {
-      ...props,
-      children: children.map(child => {
-        return typeof child === 'string'
-          ? createTextNode(child)
-          : child
-      })
-    }
-  }
-}
+
 // const tNode = createTextNode('app')
-const App = createElement('div', { id: 'app' }, 'app')
+const App = React.createElement('div', { id: 'app' }, 'app')
 // const App = createElement('div', { id: 'app' }, 'hi-', 'mini-react')
 // console.log(App)
 //#endregion -------------------- 动态生成 VDOM --------------------
@@ -41,36 +23,7 @@ const App = createElement('div', { id: 'app' }, 'app')
 // textNode.nodeValue = tNode.props.nodeValue
 // divEl.append(textNode)
 
-function render(el, container) {
-  const element = el.type === 'TEXT_ELEMENT'
-    ? document.createTextNode('')
-    : document.createElement(el.type)
-
-  // id, class, ...
-  Object.keys(el.props).forEach(prop => {
-    if (prop !== 'children') {
-      element[prop] = el.props[prop]
-    }
-  })
-
-  el.props.children.forEach(child => {
-    // 递归
-    render(child, element)
-  })
-
-  container.append(element)
-}
-
 // render(App, document.querySelector('#root'))
 
-const ReactDOM = {
-  createRoot(container) {
-    return {
-      render(App) {
-        render(App, container)
-      }
-    }
-  }
-}
 ReactDOM.createRoot(document.querySelector('#root')).render(App)
 //#endregion -------------------- 动态渲染 DOM --------------------
